1ab4ae4453897afddbbf2928f58692c87ea113bf
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2022-08-11 10:31:40 +0200

    Splits the text into letters

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const text = document.getElementById("name");
+
+const animate_text = () => {
+    // Splits the text into letters
+    const str = text.textContent;
+    const letters = str.split("");
+}
