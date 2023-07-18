2e418576907c075f31be6d844cd2b301d39af4c2
Mohamed Hafez <mohamedhafezmansour@gmail.com>
2023-07-18 19:49:55 +0300

    Convert to Typescript

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
+"use strict";
 const text = document.getElementById("name");
-
-const animate_text = () => {
-    // Splits the text into letters
-    const str = text.textContent;
-    const letters = str.split("");
-
-    // Remove the origin text
-    text.textContent = "";
-
-    // Warp every letter in a span
-    for (let le of letters) {
-        let newSpan = document.createElement("span");
-        newSpan.textContent = le;
-        text.appendChild(newSpan);
-    }
-
-    let char = 0;
-    let timer = setInterval(onTick, 130);
-
-    function onTick() {
-        const span = text.getElementsByTagName("span")[char];
-        span.classList.add('animate-text');
-        char++;
-        if (char === letters.length) {
-            complete();
-            return;
+const animate_text = (text) => {
+    if (text) {
+        // Splits the text into letters
+        const str = text.textContent !== null ? text.textContent : "";
+        const letters = str.split("");
+        // Remove the origin text
+        text.textContent = "";
+        // Warp every letter in a span
+        for (let le of letters) {
+            let newSpan = document.createElement("span");
+            newSpan.textContent = le;
+            text.appendChild(newSpan);
+        }
+        let char = 0;
+        let timer = setInterval(onTick, 130);
+        function onTick() {
+            const span = text.getElementsByTagName("span")[char];
+            span.classList.add("animate-text");
+            char++;
+            if (char === letters.length) {
+                complete();
+                return;
+            }
+        }
+        function complete() {
+            clearInterval(timer);
+            // timer = null;
         }
     }
-
-    function complete() {
-        clearInterval(timer);
-        timer = null;
-    }
-
-}
-
-animate_text();
+};
+animate_text(text);
---
+++ index.ts
@@ -0,0 +1,39 @@
+const text = document.getElementById("name")!
+
+const animate_text = (text: HTMLElement) => {
+  if (text) {
+    // Splits the text into letters
+    const str: string = text.textContent !== null ? text.textContent : ""
+    const letters = str.split("")
+
+    // Remove the origin text
+    text.textContent = ""
+
+    // Warp every letter in a span
+    for (let le of letters) {
+      let newSpan = document.createElement("span")
+      newSpan.textContent = le
+      text.appendChild(newSpan)
+    }
+
+    let char = 0
+    let timer = setInterval(onTick, 130)
+
+    function onTick() {
+      const span = text.getElementsByTagName("span")[char]
+      span.classList.add("animate-text")
+      char++
+      if (char === letters.length) {
+        complete()
+        return
+      }
+    }
+
+    function complete() {
+      clearInterval(timer)
+      // timer = null;
+    }
+  }
+}
+
+animate_text(text)
